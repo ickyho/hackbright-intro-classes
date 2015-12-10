25a25cb80b76848ea8bc67f2a332eaa147713196
Kelsey and Laura <laura.m.matchett@gmail.com>
2015-12-09 19:23:38 -0800

    refactored Class

---
+++ chapter14_driver.py
@@ -0,0 +1,12 @@
+#import codeappclasses
+
+#Ben = codeappclasses.BankAccount('Ben',100)
+#print Ben.deposit(50)
+
+#Patty = codeappclasses.BankAccount('Patty',1000)
+#print Patty.deposit(100)
+
+import food
+
+berries = food.Food('berries','purple','wash the berries')
+print berries.print_recipe()
